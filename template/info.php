7f2d338a739be88f74658f79f069e94ccd14b9df
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-08 00:22:03 +0800

    tambah-layout-info-jumlah-pengajuan-disetujui

--- template/info.php
+++ template/info.php
@@ -1,26 +1,24 @@
-<?php 
-
-  // print_r($data->setuju);die;
-
- ?>
-<div class="row d-flex justify-content-between m-2">
+<?php
+$no = 1;
+?>
+<div class="row d-flex justify-content-around m-2">
   <div class="card col-md-3">
     <div class="card-header">
       Pengajuan LDP
     </div>
     <!-- /.card-header -->
     <div class="card-body col-12">
-    
-        <div class='badge badge-success d-flex justify-content-between mb-2'>
-          <span class="h5"><b>Disetujui</b></span>
-          <span class="h5 pr-3"><b><?= $ldp->setuju ?></b></span>
-        </div>
-        
-        <div class='badge badge-danger d-flex justify-content-between'>
-          <span class="h5"><b>Ditolak</b></span>
-          <span class="h5 pr-3"><b><?= $ldp->tolak ?></b></span>
-        </div>
-        
+
+      <div class='badge badge-success d-flex justify-content-between mb-2'>
+        <span class="h5"><b>Disetujui</b></span>
+        <span class="h5 pr-3"><b><?= $ldp->setuju ?></b></span>
+      </div>
+
+      <div class='badge badge-danger d-flex justify-content-between'>
+        <span class="h5"><b>Ditolak</b></span>
+        <span class="h5 pr-3"><b><?= $ldp->tolak ?></b></span>
+      </div>
+
     </div>
     <!-- /.card-body -->
   </div>
@@ -31,21 +29,21 @@
     </div>
     <!-- /.card-header -->
     <div class="card-body">
-    
-    <div class='badge badge-success d-flex justify-content-between mb-2'>
-          <span class="h5"><b>Disetujui</b></span>
-          <span class="h5 pr-3"><b><?= $bbm->setuju ?></b></span>
-        </div>
-        
-        <div class='badge badge-danger d-flex justify-content-between'>
-          <span class="h5"><b>Ditolak</b></span>
-          <span class="h5 pr-3"><b><?= $bbm->tolak ?></b></span>
-        </div>
+
+      <div class='badge badge-success d-flex justify-content-between mb-2'>
+        <span class="h5"><b>Disetujui</b></span>
+        <span class="h5 pr-3"><b><?= $bbm->setuju ?></b></span>
+      </div>
+
+      <div class='badge badge-danger d-flex justify-content-between'>
+        <span class="h5"><b>Ditolak</b></span>
+        <span class="h5 pr-3"><b><?= $bbm->tolak ?></b></span>
+      </div>
     </div>
-    
+
     <!-- /.card-body -->
   </div>
-  
+
   <div class="card col-md-3">
     <div class="card-header">
       Pengajuan Service
@@ -53,19 +51,65 @@
     <!-- /.card-header -->
     <div class="card-body">
 
-    <div class='badge badge-success d-flex justify-content-between mb-2'>
-          <span class="h5"><b>Disetujui</b></span>
-          <span class="h5 pr-3"><b><?= $servis->setuju ?></b></span>
-        </div>
-        
-        <div class='badge badge-danger d-flex justify-content-between'>
-          <span class="h5"><b>Ditolak</b></span>
-          <span class="h5 pr-3"><b><?= $servis->tolak ?></b></span>
-        </div>
+      <div class='badge badge-success d-flex justify-content-between mb-2'>
+        <span class="h5"><b>Disetujui</b></span>
+        <span class="h5 pr-3"><b><?= $servis->setuju ?></b></span>
+      </div>
+
+      <div class='badge badge-danger d-flex justify-content-between'>
+        <span class="h5"><b>Ditolak</b></span>
+        <span class="h5 pr-3"><b><?= $servis->tolak ?></b></span>
+      </div>
     </div>
 
-    </div>
-    <!-- /.card-body -->
   </div>
+  <!-- /.card-body -->
+</div>
 
+<div class="row d-flex justify-content-around m-2">
+  <div class="card col-md-5">
+    <div class="card-header">
+      Data mobil yang telah diservis
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+
+      <table class="table table-striped">
+        <tbody>
+          <?php foreach ($jumlah_servis as $dt) { ?>
+            <tr>
+              <th scope="row"><?= $no++; ?></th>
+              <td><?= $dt->jenis_kendaraan ?></td>
+              <td><?= $dt->jumlah_servis ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+
+    </div>
+
+  </div>
+  <div class="card col-md-5">
+    <div class="card-header">
+      Data mobil yang telah diisi bensin
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+
+      <table class="table table-striped">
+        <tbody>
+          <?php foreach ($jumlah_isi_bbm as $dt) { ?>
+            <tr>
+              <th scope="row"><?= $no++; ?></th>
+              <td><?= $dt->jenis_kendaraan ?></td>
+              <td><?= $dt->jumlah_isi ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+
+    </div>
+
+  </div>
+  <!-- /.card-body -->
 </div>

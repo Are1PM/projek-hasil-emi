804e9a4b9ff2253a67c3edeef98f3b839cf940f7
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-10 13:15:59 +0800

    Tambah-tampilan-detail-info

---
+++ template/detail-info.php
@@ -0,0 +1,19 @@
+<?php
+$no_servis = 1;
+$no_bbm = 1;
+?>
+<div class="row d-flex justify-content-around m-2">
+  <div class="card col-md-12">
+    <div class="card-header">
+      Detail - Informasi
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body col-12">
+
+
+
+    </div>
+    <!-- /.card-body -->
+  </div>
+
+</div>

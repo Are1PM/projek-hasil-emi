93fbbe65b5f4295768fc5eb9cc0a327786040b88
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-12 14:18:05 +0800

    Perbaikan-kesalahan-kode

--- apps/pengajuan/kupon-bbm/data.php
+++ apps/pengajuan/kupon-bbm/data.php
@@ -1,75 +1,77 @@
 <div class="card">
   <div class="card-header">
-  <?php
-            if ($_SESSION['hak_akses']!="unit_sla") { ?>
-    <a href="?emi=tambah-pengajuanbbm" class="btn btn-sm btn-success"><i class="fa fa-plus"></i> Tambah data</a>
+    <?php
+    if ($_SESSION['hak_akses'] != "unit_sla") { ?>
+      <a href="?emi=tambah-pengajuanbbm" class="btn btn-sm btn-success"><i class="fa fa-plus"></i> Tambah data</a>
     <?php } ?>
   </div>
   <div class="card-body">
     <table id="example1" class="table table-bordered table-striped">
       <thead>
-      <tr>
-        <th>Tanggal pengajuan</th>
-        <th>Tanggal pengisian</th>
-        <th>Jarak tempuh</th>
-        <th>Voucher</th>
-        <th>Jenis BBM</th>
-        <th>Driver</th>
-        <th>SPBU</th>
-        <th>Kendaraan</th>
-        <th>id_status</th>
-        <th width="13%">Opsi</th>
-      </tr>
+        <tr>
+          <th>Tanggal pengajuan</th>
+          <th>Tanggal pengisian</th>
+          <th>Jarak tempuh</th>
+          <th>Voucher</th>
+          <th>Jenis BBM</th>
+          <th>Driver</th>
+          <th>SPBU</th>
+          <th>Kendaraan</th>
+          <th>id_status</th>
+          <th width="13%">Opsi</th>
+        </tr>
       </thead>
       <tbody>
-        <?php 
-         foreach ($data_pengajuanbbm as $data) { ?>
-        <tr>
-          <td><?= $data->tanggal_pengajuan ?></td>
-          <td><?= $data->tanggal_pengisian ?></td>
-          <td><?= $data->jarak_tempuh ?></td>
-          <td><?= $data->voucher ?></td>
-          <td><?= $data->jenis_bbm ?></td>
-          <td><?= $data->nama_driver ?></td>
-          <td><?= $data->nama_spbu ?></td>
-          <td><?= $data->jenis_kendaraan ?></td>
-          <td>
-          <?php 
-          if ($data->id_status == 0) {
-            echo "<span class='badge badge-warning'>Belum Dikirim</span>";
-          }elseif ($data->id_status == 4){
-            echo "<span class='badge badge-success'>Menunggu verifikasi admin</span>";
-          }  ?>
-         
-          <td>
-          <?php
-            if ($_SESSION['hak_akses']!="unit_sla") { ?>
-             <?php
-                if ($data->id_status==0) { ?>
-                   <a href="?emi=ubah-pengajuanbbm&id_pengajuan_kupon_bbm=<?= $data->id_pengajuan_kupon_bbm; ?>&parameter=1" class="btn btn-sm btn-warning"><i class="fa fa-pen"></i> Edit</a> |
-            <?php } ?>
-            <?php
-                if ($data->id_status==0) { ?>
-                   <a href="?emi=hapus-pengajuanbbm&id_pengajuan_kupon_bbm=<?= $data->id_pengajuan_kupon_bbm ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Hapus</a> |
-            <?php } ?>
+        <?php
+        foreach ($data_pengajuanbbm as $data) { ?>
+          <tr>
+            <td><?= $data->tanggal_pengajuan ?></td>
+            <td><?= $data->tanggal_pengisian ?></td>
+            <td><?= $data->jarak_tempuh ?></td>
+            <td><?= $data->voucher ?></td>
+            <td><?= $data->jenis_bbm ?></td>
+            <td><?= $data->nama_driver ?></td>
+            <td><?= $data->nama_spbu ?></td>
+            <td><?= $data->jenis_kendaraan ?></td>
+            <td>
+              <?php
+              if ($data->id_status == 0) {
+                echo "<span class='badge badge-warning'>Belum Dikirim</span>";
+              } elseif ($data->id_status == 4) {
+                echo "<span class='badge badge-success'>Menunggu verifikasi admin</span>";
+              }  ?>
 
-            <?php } ?>
-            
+            <td>
+              <?php
+              if ($_SESSION['hak_akses'] != "unit_sla") { ?>
+                <?php
+                if ($data->id_status == 0) { ?>
+                  <a href="?emi=ubah-pengajuanbbm&id_pengajuan_kupon_bbm=<?= $data->id_pengajuan_kupon_bbm; ?>&parameter=1" class="btn btn-sm btn-warning"><i class="fa fa-pen"></i> Edit</a> |
+                <?php } ?>
+                <?php
+                if ($data->id_status == 0) { ?>
+                  <a href="?emi=hapus-pengajuanbbm&id_pengajuan_kupon_bbm=<?= $data->id_pengajuan_kupon_bbm ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Hapus</a> |
+                <?php } ?>
+
+              <?php } ?>
+
               <a href="?emi=detail-pengajuanbbm&id_pengajuan_kupon_bbm=<?= $data->id_pengajuan_kupon_bbm; ?>&parameter=1" class="btn btn-sm btn-info"><i class="fa fa-eye"></i> Detail</a>
 
-            <?php
-            if ($_SESSION['hak_akses']!="driver") { ?>  |
-            <a href="#" class="konfirmasi" data="<?= $data->id_pengajuan_kupon_bbm ?>" class="btn btn-sm btn-info">Konfirmasi</a>
-            <?php } ?> 
-            <?php
-            if ($_SESSION['hak_akses']=="driver") { ?>
-            <?php
-              if ($data->id_status==0) { ?>
-             |
-            <a href="apps/pengajuan/kupon-bbm/proses-kirim-data.php?id_pengajuan_bbm=<?= $data->id_pengajuan_kupon_bbm; ?>" class="btn btn-sm btn-primary">Kirim</a>
-            <?php } } }?> 
-          </td>
-        </tr>
+              <?php
+              if ($_SESSION['hak_akses'] != "driver") { ?> |
+                <a href="#" class="konfirmasi" data="<?= $data->id_pengajuan_kupon_bbm ?>" class="btn btn-sm btn-info">Konfirmasi</a>
+              <?php } ?>
+              <?php
+              if ($_SESSION['hak_akses'] == "driver") { ?>
+                <?php
+                if ($data->id_status == 0) { ?>
+                  |
+                  <a href="apps/pengajuan/kupon-bbm/proses-kirim-data.php?id_pengajuan_bbm=<?= $data->id_pengajuan_kupon_bbm; ?>" class="btn btn-sm btn-primary">Kirim</a>
+            <?php }
+              }
+            } ?>
+            </td>
+          </tr>
       </tbody>
     </table>
   </div>
@@ -77,9 +79,9 @@
 <div class="modal fade" id="data"></div>
 
 <?php
-  if (isset($_GET['pesan'])) {
-    if ($_GET['pesan']=="berhasil") {
-      echo '<div id="myModal" class="modal fade" role="dialog">
+if (isset($_GET['pesan'])) {
+  if ($_GET['pesan'] == "berhasil") {
+    echo '<div id="myModal" class="modal fade" role="dialog">
               <div class="modal-dialog">
                   <div class="modal-content">
                       <div class="modal-header">
@@ -98,6 +100,6 @@
                   </div>
               </div>
           </div>';
-      }
-    }
+  }
+}
 ?>

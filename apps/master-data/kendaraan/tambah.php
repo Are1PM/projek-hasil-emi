0969507c54ab17a9fdab8eb4b90d5b98f2491959
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-12 18:58:49 +0800

    Ubah-nilai-default-status

--- apps/master-data/kendaraan/tambah.php
+++ apps/master-data/kendaraan/tambah.php
@@ -1,32 +1,32 @@
 <div class="row">
     <!-- left column -->
     <div class="col-md-12">
-    <!-- general form elements -->
-    <div class="card card-primary">
-        <div class="card-header">
-        <h3 class="card-title">Tambah data kendaraan</h3>
-        </div>
-        <!-- /.card-header -->
-        <!-- form start -->
-        <form role="form" method="post" action="">
-        <div class="card-body">
-            <div class="form-group">
-            <label for="jenis_kendaraan">Jenis kendaraan</label>
-            <input type="text" class="form-control" name="jenis_kendaraan" id="jenis_kendaraan" placeholder="Enter jenis kendaraan">
+        <!-- general form elements -->
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Tambah data kendaraan</h3>
             </div>
-            <div class="form-group">
-            <label for="plat">Plat</label>
-            <input type="text" class="form-control" name="plat" id="username" placeholder="Enter plat">
-            </div>
-            <div class="form-group">
-            <label for="tanggal_masuk">Tanggal masuk</label>
-            <input type="date" class="form-control" name="tanggal_masuk" id="tanggal_masuk" placeholder="Enter tanggal masuk">
-            </div>
-            <!-- <div class="form-group">
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form role="form" method="post" action="">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="jenis_kendaraan">Jenis kendaraan</label>
+                        <input type="text" class="form-control" name="jenis_kendaraan" id="jenis_kendaraan" placeholder="Enter jenis kendaraan">
+                    </div>
+                    <div class="form-group">
+                        <label for="plat">Plat</label>
+                        <input type="text" class="form-control" name="plat" id="username" placeholder="Enter plat">
+                    </div>
+                    <div class="form-group">
+                        <label for="tanggal_masuk">Tanggal masuk</label>
+                        <input type="date" class="form-control" name="tanggal_masuk" id="tanggal_masuk" placeholder="Enter tanggal masuk">
+                    </div>
+                    <!-- <div class="form-group">
             <label for="no_hp">No HP</label>
             <input type="text" class="form-control" name="no_hp" id="no_hp" placeholder="Enter no hp">
             </div> -->
-            </div>
+                </div>
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
@@ -34,19 +34,19 @@
         </div>
         </form>
         <?php
-            if (isset($_POST['simpan'])) {
-                $jenis_kendaraan=$_POST['jenis_kendaraan'];
-                $plat=$_POST['plat'];
-                $tanggal_masuk=$_POST['tanggal_masuk'];
-                $id_status_kendaraan='ready';
+        if (isset($_POST['simpan'])) {
+            $jenis_kendaraan = $_POST['jenis_kendaraan'];
+            $plat = $_POST['plat'];
+            $tanggal_masuk = $_POST['tanggal_masuk'];
+            $id_status_kendaraan = 1;
 
-                $proses = new MengelolaKendaraan('',$jenis_kendaraan,$plat,$tanggal_masuk,$id_status_kendaraan);
-                $input = $proses->MemasukkanKendaraan();
-            }
+            $proses = new MengelolaKendaraan('', $jenis_kendaraan, $plat, $tanggal_masuk, $id_status_kendaraan);
+            $input = $proses->MemasukkanKendaraan();
+        }
         ?>
     </div>
     <!-- /.card -->
 
-    </div>
-    <!--/.col (left) -->
+</div>
+<!--/.col (left) -->
 </div>
